c9f50a42d0da398f9ecd8732e2719f9ac315d22f
Joseph Montero <3136168+josephma93@users.noreply.github.com>
2024-07-04 11:19:54 -0600

    add get-article enpoint and docs

--- README.md
+++ README.md
@@ -1,2 +1,116 @@
 # pub-w-tools
-Just some code I use to do stuff with pub-w texts
+
+`pub-w-tools` is a set of tools designed to handle and process HTML content from W articles on the WOL site.
+The primary functions include parsing the HTML of Watchtower articles into a structured JSON
+format and retrieving the HTML of the current week's article directly from the website.
+
+## Features
+
+- **HTML to JSON Parsing:**
+    - Receives HTML content of a Watchtower article.
+    - Parses the HTML to extract key information and converts it into a structured JSON format for further processing.
+- **Weekly Article Retrieval:**
+    - Automatically fetches the HTML content of the current week's Watchtower article from the WO.
+
+## Disclaimer
+
+This tool is intended for personal use only. The use of this tool must comply with the terms and conditions of the WOL.
+Users are prohibited from redistributing, using the content for commercial purposes, or posting the content on any other site.
+Please review the WOL's Terms and Conditions of Use before using this tool to ensure compliance.
+
+## Installation
+
+1. Clone the repository:
+    ```bash
+    git clone https://github.com/yourusername/pub-w-tools.git
+    cd pub-w-tools
+    ```
+
+2. Install the required dependencies:
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+## Usage
+
+### Starting the Flask App
+
+To start the Flask application, run:
+
+```bash
+python pub_w_tools.py
+```
+
+By default, the app will run on `http://0.0.0.0:3001`.
+
+### Endpoints
+
+#### 1. `/pub-w` (POST)
+
+This endpoint accepts HTML content of a Watchtower article and returns a structured JSON representation.
+
+- **URL:** `/pub-w`
+- **Method:** `POST`
+- **Content-Type:** `application/x-www-form-urlencoded`
+- **Body Parameter:**
+    - `html` (string): The HTML content of the article.
+
+- **Response:**
+    - `200 OK` with JSON containing the parsed article.
+    - `400 Bad Request` if the input HTML is not provided.
+
+**Example Request:**
+
+```bash
+curl -X POST http://0.0.0.0:3001/pub-w -d "html=<html_content>"
+```
+
+#### 2. `/get-article` (GET)
+
+This endpoint fetches the HTML of the current week's Watchtower article from the WOL.
+
+- **URL:** `/get-article`
+- **Method:** `GET`
+
+- **Response:**
+    - `200 OK` with the HTML content of the article.
+    - Appropriate error messages and status codes if the article is not found or other errors occur during processing.
+
+**Example Request:**
+
+```bash
+curl http://0.0.0.0:3001/get-article
+```
+
+## Docker
+
+The application is available as a Docker image on Docker Hub.
+
+### Pulling the Latest Image
+
+```bash
+docker pull pub-w-tools:latest
+```
+
+### Running the Docker Container
+
+```bash
+docker run -d -p 3001:3001 pub-w-tools:latest
+```
+
+### Versioned Images
+
+Versioned images are also available using the commit SHA as the tag:
+
+```bash
+docker pull pub-w-tools:{{ SHORT_SHA }}
+docker run -d -p 3001:3001 pub-w-tools:{{ SHORT_SHA }}
+```
+
+## Contributing
+
+Contributions are welcome! Please open an issue or submit a pull request for any improvements or bug fixes.
+
+## License
+
+See the [LICENSE](./LICENSE) file for details.
--- pub_w_tools.py
+++ pub_w_tools.py
@@ -1,9 +1,11 @@
 import logging
 import os
 import re
+import time
 from typing import Any, Dict, List
 from bs4 import BeautifulSoup
 from flask import Flask, request, jsonify
+import requests
 
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -19,7 +21,7 @@ def extract_paragraph_numbers(question) -> List[int]:
     return []
 
 
-def get_question_text_clean(question) -> str:
+def remove_strong_tag(question) -> str:
     strong_tag = question.find('strong')
     if strong_tag:
         strong_tag.decompose()
@@ -38,7 +40,7 @@ def parse_html_to_json(html: str) -> Dict[str, Any]:
     questions = soup.find_all('p', class_='qu')
     for question in questions:
         p_numbers = extract_paragraph_numbers(question)
-        q_text = get_question_text_clean(question)
+        q_text = remove_strong_tag(question)  # Remove the paragraph number and following space
         paragraphs = []
 
         # Get related paragraphs using data-pid and data-rel-pid
@@ -75,6 +77,25 @@ def parse_html_to_json(html: str) -> Dict[str, Any]:
     return json_data
 
 
+def get_html_content(url: str) -> (str, int):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.text, 200
+    except requests.exceptions.HTTPError as e:
+        logger.error(f'HTTP error occurred: {e.response.status_code} - {e.response.reason}')
+        return f'HTTP error: {e.response.status_code} - {e.response.reason}', e.response.status_code
+    except requests.exceptions.ConnectionError as e:
+        logger.error(f'Connection error occurred: {e}')
+        return 'Connection error occurred', 503
+    except requests.exceptions.Timeout as e:
+        logger.error(f'Timeout error occurred: {e}')
+        return 'Timeout error occurred', 504
+    except requests.exceptions.RequestException as e:
+        logger.error(f'Request error occurred: {e}')
+        return 'Request error occurred', 500
+
+
 @app.route('/pub-w', methods=['POST'])
 def parse_pub_w():
     input_html = request.form.get('html')
@@ -85,6 +106,85 @@ def parse_pub_w():
     json_data = parse_html_to_json(input_html)
     logger.info('Successfully parsed HTML to JSON.')
     return jsonify(json_data), 200
+
+
+@app.route('/get-article', methods=['GET'])
+def get_article():
+    try:
+        start_time = time.time()
+
+        # Step 1: Request to https://wol.jw.org
+        base_url = 'https://wol.jw.org'
+        logger.info('Step 1: Requesting base URL')
+        html_content, status_code = get_html_content(base_url)
+        if status_code != 200:
+            return jsonify({'error': html_content}), status_code
+        soup = BeautifulSoup(html_content, 'html5lib')
+        logger.info(f'Step 1 completed in {time.time() - start_time:.2f} seconds')
+
+        # Step 2: Extract href from link[hreflang="es"]
+        step_start_time = time.time()
+        href_lang_es = soup.select_one('link[hreflang="es"]')
+        if not href_lang_es:
+            return jsonify({'error': 'No href found for hreflang="es"'}), 404
+        href_lang_es = href_lang_es['href']
+        logger.info(f'Step 2 completed in {time.time() - step_start_time:.2f} seconds')
+
+        # Step 3: Request to the extracted href
+        step_start_time = time.time()
+        html_content, status_code = get_html_content(base_url + href_lang_es)
+        if status_code != 200:
+            return jsonify({'error': html_content}), status_code
+        soup = BeautifulSoup(html_content, 'html5lib')
+        logger.info(f'Step 3 completed in {time.time() - step_start_time:.2f} seconds')
+
+        # Step 4: Extract href from #menuToday .todayNav
+        step_start_time = time.time()
+        today_nav = soup.select_one('#menuToday .todayNav')
+        if not today_nav:
+            return jsonify({'error': 'No href found for #menuToday .todayNav'}), 404
+        today_nav_href = today_nav['href']
+        logger.info(f'Step 4 completed in {time.time() - step_start_time:.2f} seconds')
+
+        # Step 5: Request to the extracted href
+        step_start_time = time.time()
+        html_content, status_code = get_html_content(base_url + today_nav_href)
+        if status_code != 200:
+            return jsonify({'error': html_content}), status_code
+        soup = BeautifulSoup(html_content, 'html5lib')
+        logger.info(f'Step 5 completed in {time.time() - step_start_time:.2f} seconds')
+
+        # Step 6: Extract href from .todayItem.pub-w:nth-child(2) .itemData a
+        step_start_time = time.time()
+        pub_w_item = soup.select_one('.todayItem.pub-w:nth-child(2) .itemData a')
+        if not pub_w_item:
+            return jsonify({'error': 'No href found for .todayItem.pub-w:nth-child(2) .itemData a'}), 404
+        pub_w_item_href = pub_w_item['href']
+        logger.info(f'Step 6 completed in {time.time() - step_start_time:.2f} seconds')
+
+        # Step 7: Request to the extracted href
+        step_start_time = time.time()
+        html_content, status_code = get_html_content(base_url + pub_w_item_href)
+        if status_code != 200:
+            return jsonify({'error': html_content}), status_code
+        soup = BeautifulSoup(html_content, 'html5lib')
+        logger.info(f'Step 7 completed in {time.time() - step_start_time:.2f} seconds')
+
+        # Step 8: Extract and return the HTML of the element with id="article"
+        step_start_time = time.time()
+        article_element = soup.find(id='article')
+        if not article_element:
+            return jsonify({'error': 'No element found with id="article"'}), 404
+        logger.info(f'Step 8 completed in {time.time() - step_start_time:.2f} seconds')
+
+        total_time = time.time() - start_time
+        logger.info(f'Total time for get_article: {total_time:.2f} seconds')
+
+        return str(article_element), 200
+
+    except Exception as e:
+        logger.error(f'Error in get_article endpoint: {e}')
+        return jsonify({'error': 'An error occurred while processing your request'}), 500
 
 
 if __name__ == '__main__':
